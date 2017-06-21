3df150c7a0cc478d329e9b951a36795a9e84afa0
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-06-21 22:40:17 +0800

    photo route 1. refactor the code 2. added comments to the code

--- src/server/routes/api/photos.js
+++ src/server/routes/api/photos.js
@@ -11,36 +11,40 @@ import routerResponse from '../../utilities/routerResponse'
 const router = express.Router()
 const upload = multer({ dest: path.resolve('./dist/client/upload/') })
 
-router.post('/', upload.array('prod-photo', null), (req, res) => {
-    // console.log(req.files)
-    let dbQueries = []
-    req.files.forEach((file) => {
+router
+    .post('/', upload.array('prod-photo', null), processPhotoUploads)
+
+module.exports = router
+
+// deal with photo uploads and place inside the database
+function processPhotoUploads(req, res, next) {
+    let dbQueries = [] // array to hold queries that writes photo to database
+    req.files.forEach((file) => { // place all uploads into the array
         dbQueries.push(
-            db.Photos.create({
+            db.Photos.create({ // sequelize insert query
                 originalName: file.originalname,
                 encoding: file.encoding,
                 mimetype: file.mimetype,
                 size: file.size,
-                photoData: fs.readFileSync(file.path)
+                photoData: fs.readFileSync(file.path) // actual photo data is written to a blob field
             })
         )
     })
-    return Promise.all(dbQueries)
+    return Promise.all(dbQueries) // run the queries
         .then((dbQueryResult) => {
-            req.files.forEach((file) => {
+            req.files.forEach((file) => { // remove all the left over photos
                 del.sync(file.path)
             })
-            return routerResponse.json({
+            return routerResponse.json({ // return a successful photo upload
                 pendingResponse: res,
                 originalRequest: req,
                 statusCode: 200,
                 success: true,
-                data: dbQueryResult
+                data: dbQueryResult // actual data return by the insert queries about the photo records
             })
         })
         .catch((error) => {
-            // console.log(error)
-            return routerResponse.json({
+            return routerResponse.json({ // return error object to the frontend when the photo upload had failed
                 pendingResponse: res,
                 originalRequest: req,
                 statusCode: 500,
@@ -49,6 +53,4 @@ router.post('/', upload.array('prod-photo', null), (req, res) => {
                 message: 'photo upload error'
             })
         })
-})
-
-module.exports = router
+}
