2e688ec3064ffc7d979b993ba6b6bde740d851b3
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-15 08:23:48 +0800

    prettier formatting

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,11 @@ module.exports = {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: pkg.description},
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Product Catalog Website of Gentry Way Co., Ltd.',
+      },
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
