8d3302369b7eeb9cba6b9a4b7e5f565c26ee2895
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-15 10:49:04 +0800

    bug fix: 1. content component on 'Home Page' was incorrectly set to 'Page' 2. other insignificant coding style update

--- bin/libs/setupHomePage.js
+++ bin/libs/setupHomePage.js
@@ -4,26 +4,26 @@ const api = require('./storyblokApi')
 const seedData = require('./seedData').getCarouselSlides()
 
 module.exports = () => {
+  let homeFolderInfo = {
+    story: {
+      name: 'Home Folder',
+      slug: 'home-page', // used as start page slug (storyblok behavior)
+      is_folder: true,
+      default_root: 'Home Page',
+      parent_id: 0,
+    },
+  }
   let homePageInfo = {
     story: {
       name: 'Home Page',
-      slug: 'home-page', // ignored by storyblok
+      slug: homeFolderInfo.story.slug, // ignored by storyblok
       content: {
-        component: 'Page',
+        component: 'Home Page',
         carouselSlides: [],
       },
       path: '/',
       is_startpage: true,
       parent_id: '',
-    },
-  }
-  let homeFolderInfo = {
-    story: {
-      name: 'Home Folder',
-      slug: homePageInfo.story.slug, // used as start page slug (storyblok behavior)
-      is_folder: true,
-      default_root: 'Home Page',
-      parent_id: 0,
     },
   }
   let carouselSlides = homePageInfo.story.content.carouselSlides
