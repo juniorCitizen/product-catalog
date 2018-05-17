2b51f1c6accd46a973492896cc677c7ca7a5910a
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-17 10:31:08 +0800

    feature - enable local server to be started on url other than '127.0.0.1' or 'localhost'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+require('dotenv-safe').config()
 
 const express = require('express')
-const { Nuxt, Builder } = require('nuxt')
+const {Nuxt, Builder} = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
