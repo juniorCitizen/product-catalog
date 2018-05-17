8fe6bc3558e6dffbd6c90155d243f290feb9e82c
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-17 10:29:38 +0800

    feature - in-progress component now work with scrollable pages

--- components/InProgress.vue
+++ components/InProgress.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="in-progress">
+  <div id="in-progress"
+       :style="topPos">
     <div class="caption-text">
       <span class="icon has-text-warning">
         <i class="fas fa-2x fa-spinner fa-pulse"/>
@@ -14,6 +15,20 @@
 <script>
 export default {
   name: 'InProgress',
+  data() {
+    return {yOffset: 0}
+  },
+  computed: {
+    topPos() {
+      return {top: `${this.yOffset}px`}
+    },
+  },
+  mounted() {
+    this.yOffset = window.pageYOffset
+    window.addEventListener('scroll', () => {
+      this.yOffset = window.pageYOffset
+    })
+  },
 }
 </script>
 
@@ -21,7 +36,6 @@ export default {
 #in-progress {
   z-index: 1000;
   position: absolute;
-  top: 0;
   left: 0;
   height: 100vh;
   width: 100vw;
